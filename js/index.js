5f46f3856234233ba26a450c994cfb939c3b280d
EurisG <euris809@gmail.com>
2022-05-03 15:36:22 -0400

    starting javascript pseudocode

---
+++ js/index.js
@@ -0,0 +1,8 @@
+// const math = document.querySelector('.math');
+// const history = document.querySelector('.history');
+// const science = document.querySelector('.science');
+// const music = document.querySelector('.music');
+// const sports = document.querySelector('.sports');
+// const start = document.querySelector('.sBtn')
+// const restart = document.querySelector('.rBtn')
+// let scoreBoard = 0
