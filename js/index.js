c1b5bd69a4afbdcadb8dfff84a3c92afc4c87a84
EurisG <euris809@gmail.com>
2022-05-04 12:38:35 -0400

    added the math questions

--- js/index.js
+++ js/index.js
@@ -14,6 +14,12 @@ let playerScoreTracker = {
     false: 0
 }
 
+window.timerBox = function(){
+    var seconds = 00;
+    var appendSeconds = document.querySelector('.timer')
+    // sBtn.onclick = function()
+} 
+
 
 
 // start button function 
@@ -40,6 +46,9 @@ function startGame() {
 
 // jeopardy question sysytem 
 
+// -------------Math Questions -------------
+
+    // math question 1
 const  toggleMathOne = () => {
     
     let mathRiddleOne = prompt(' 30 + 40 = ? ')
@@ -63,11 +72,117 @@ const  toggleMathOne = () => {
         isPlayerTurn = !isPlayerTurn
         
     }
-
-
     console.log(playerScoreTracker)
 }
 
+
+    // math question 2
+    const  toggleMathTwo = () => {
+    
+        let mathRiddleTwo = prompt(' 60 / ? = 5 ')
+        if (mathRiddleTwo === '12') {
+            alert('Correct! Player goes again !')
+            playerScoreTracker[isPlayerTurn] += 200
+            if (isPlayerTurn === true) {
+                document.querySelector('.scoreBoardOne').innerHTML = playerScoreTracker[isPlayerTurn]
+            } else {
+                 document.querySelector('.scoreBoardTwo').innerHTML = playerScoreTracker[isPlayerTurn]
+            }
+           
+        } else {
+            alert('Incorrect! Next player goes !')
+            playerScoreTracker[isPlayerTurn] -= 50
+            if (isPlayerTurn === true) {
+                document.querySelector('.scoreBoardOne').innerHTML = playerScoreTracker[isPlayerTurn]
+            } else {
+                 document.querySelector('.scoreBoardTwo').innerHTML = playerScoreTracker[isPlayerTurn]
+            }
+            isPlayerTurn = !isPlayerTurn
+            
+        }
+        console.log(playerScoreTracker)
+    }
+
+    // math question 3
+    const  toggleMathThree = () => {
+    
+        let mathRiddleThree = prompt(' 9 * 12 = ? ')
+        if (mathRiddleThree === '108') {
+            alert('Correct! Player goes again !')
+            playerScoreTracker[isPlayerTurn] += 300
+            if (isPlayerTurn === true) {
+                document.querySelector('.scoreBoardOne').innerHTML = playerScoreTracker[isPlayerTurn]
+            } else {
+                 document.querySelector('.scoreBoardTwo').innerHTML = playerScoreTracker[isPlayerTurn]
+            }
+           
+        } else {
+            alert('Incorrect! Next player goes !')
+            playerScoreTracker[isPlayerTurn] -= 50
+            if (isPlayerTurn === true) {
+                document.querySelector('.scoreBoardOne').innerHTML = playerScoreTracker[isPlayerTurn]
+            } else {
+                 document.querySelector('.scoreBoardTwo').innerHTML = playerScoreTracker[isPlayerTurn]
+            }
+            isPlayerTurn = !isPlayerTurn
+            
+        }
+        console.log(playerScoreTracker)
+    }
+
+        // math question 4 
+    const  toggleMathFour = () => {
+    
+        let mathRiddleFour = prompt(' 55 * 3 = ? ')
+        if (mathRiddleFour === '165') {
+            alert('Correct! Player goes again !')
+            playerScoreTracker[isPlayerTurn] += 400
+            if (isPlayerTurn === true) {
+                document.querySelector('.scoreBoardOne').innerHTML = playerScoreTracker[isPlayerTurn]
+            } else {
+                 document.querySelector('.scoreBoardTwo').innerHTML = playerScoreTracker[isPlayerTurn]
+            }
+           
+        } else {
+            alert('Incorrect! Next player goes !')
+            playerScoreTracker[isPlayerTurn] -= 50
+            if (isPlayerTurn === true) {
+                document.querySelector('.scoreBoardOne').innerHTML = playerScoreTracker[isPlayerTurn]
+            } else {
+                 document.querySelector('.scoreBoardTwo').innerHTML = playerScoreTracker[isPlayerTurn]
+            }
+            isPlayerTurn = !isPlayerTurn
+            
+        }
+        console.log(playerScoreTracker)
+    }
+
+   // math question 5 
+   const  toggleMathFive = () => {
+    
+    let mathRiddleFive = prompt("Shane's mom said to take a break after lunch at 12:25 p.m. His break was over at 1:10 p.m. How long was his break?")
+    if (mathRiddleFive === '45 min') {
+        alert('Correct! Player goes again !')
+        playerScoreTracker[isPlayerTurn] += 500
+        if (isPlayerTurn === true) {
+            document.querySelector('.scoreBoardOne').innerHTML = playerScoreTracker[isPlayerTurn]
+        } else {
+             document.querySelector('.scoreBoardTwo').innerHTML = playerScoreTracker[isPlayerTurn]
+        }
+       
+    } else {
+        alert('Incorrect! Next player goes !')
+        playerScoreTracker[isPlayerTurn] -= 50
+        if (isPlayerTurn === true) {
+            document.querySelector('.scoreBoardOne').innerHTML = playerScoreTracker[isPlayerTurn]
+        } else {
+             document.querySelector('.scoreBoardTwo').innerHTML = playerScoreTracker[isPlayerTurn]
+        }
+        isPlayerTurn = !isPlayerTurn
+        
+    }
+    console.log(playerScoreTracker)
+}
 
 // points system with questions 
 
